0673d5d50260f73892003b7247dc54f87584c521
NikitaEdin <nikita.lanetsky.uk@gmail.com>
2025-07-17 18:13:19 +0100

    Integrate DuckDuckGo and Wikipedia tools into agent workflow
    Added DuckDuckGo and Wikipedia tools to the agent

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 from dotenv import load_dotenv
 from pydantic import BaseModel
-from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
 from langchain.agents import create_tool_calling_agent, AgentExecutor
+from tools import search_tool, wiki_tool
 
-
+# Load environment variables from .env file
 load_dotenv()
 
+# Define Pydantic model for research response
 class ResearchResponse(BaseModel):
     topic: str
     summary: str
     source: list[str]
     tools_used: list[str]
     
-
+# Initialise LLM with Claude 4
 llm = ChatAnthropic(model="claude-opus-4-20250514",
         max_tokens=1000,
         temperature=1)
 
+
 parser = PydanticOutputParser(pydantic_object=ResearchResponse)
 
+# Define prompt template
 prompt = ChatPromptTemplate.from_messages(
     [
         (
@@ -38,28 +41,40 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 ).partial(format_instructions=parser.get_format_instructions())
     
+# Define tools to be used by agent
+tools = [search_tool, wiki_tool]
+
+
+# Create agent with LLM and prompt
 agent = create_tool_calling_agent(
     llm = llm,
     prompt = prompt,
-    tools=[]
+    tools=tools
 )
 
-agent_executor = AgentExecutor(agent=agent, tools=[],verbose=True)
+agent_executor = AgentExecutor(agent=agent, tools=tools,verbose=True)
 
+# Ask user for input query
+query = input("Enter your research query: ")
+
+# Example query to the agent
 raw_response = agent_executor.invoke({
-    "query": "What is the capital of Scotland?",
+    "query": query,
     "name": "Research Assistant"
 })
 
-#print(raw_response)
+# Print the raw response from the agent
+# Uncomment the next line to see the raw response
+### print(raw_response)
 
 # Parse the raw response using the Pydantic parser
 try:
     structured_response = parser.parse(raw_response['output'][0]["text"])
+    print(structured_response)
 except Exception as e:
-    structured_response = f"Error parsing response: {e}, raw response: {raw_response}"
+    print(f"Error parsing response: {e}, raw response: {raw_response}")
     
-print(structured_response)
+
 
 
 
--- tools.py
+++ tools.py
@@ -3,10 +3,14 @@ from langchain_community.utilities import WikipediaAPIWrapper
 from langchain.tools import Tool
 from datetime import datetime
 
+# Define search tool using DuckDuckGo
 search = DuckDuckGoSearchRun()
-searchtool = Tool(
-    name="DuckDuckGo_Search",
+search_tool = Tool(
+    name="DuckDuckGoSearch",
     func=search.run,
     description="Useful for searching the web for information on a topic."
 )
 
+# Define Wikipedia
+api_wrapper = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=100)
+wiki_tool = WikipediaQueryRun(api_wrapper=api_wrapper)
